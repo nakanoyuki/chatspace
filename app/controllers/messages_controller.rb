b27dd3a1937172431e5ad49c81f7ed329ae75d84
nakanoyuki <yukich41@gmail.com>
2019-05-22 20:51:46 +0900

    edit message.js

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,14 @@ class MessagesController < ApplicationController
   
     def create
       @message = @group.messages.new(message_params)
+      
+
+     
       if @message.save
-        redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+        respond_to do |format|
+          format.html { redirect_to group_messages_path(@group), method: :post}
+          format.json
+        end
       else
         @messages = @group.messages.includes(:user)
         flash.now[:alert] = 'メッセージを入力してください。'
@@ -17,13 +23,18 @@ class MessagesController < ApplicationController
       end
     end
   
+    def set_group
+      @group = Group.find(params[:group_id])
+    end
+
+  
     private
   
     def message_params
       params.require(:message).permit(:content,:image).merge(user_id: current_user.id)
     end
   
-    def set_group
-      @group = Group.find(params[:group_id])
-    end
+    
 end
+
+
