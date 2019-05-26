7f88e07ca19dbc532ee23a9744fdfb480cfb2231
nakanoyuki <yukich41@gmail.com>
2019-05-26 20:06:43 +0900

    edit search_user

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,11 +5,10 @@ $(function() {
 
   function appendUser(user){
 
-    var html = 
-    `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${user.name}</p>
-    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-    </div>`
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
 
     search_user.append(html);
   }
@@ -19,15 +18,13 @@ $(function() {
     search_user.append(html);
   }
 
-
   function AppendUsermemberList(name,user_id){
     
-    var html = 
-    `<div class="chat-group-user clearfix js-chat-member" id="${user_id}">
-      <input name="group[user_ids][]" type="hidden" value="${user_id}">
-      <p class="chat-group-user__name">${name}</p>
-      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
-      </div>`
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="${user_id}">
+                  <input name="group[user_ids][]" type="hidden" value="${user_id}">
+                  <p class="chat-group-user__name">${name}</p>
+                  <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
+                </div>`
 
     member_user.append(html);
   }
