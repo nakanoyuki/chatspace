b412ff527481c1116240629530a951178f9e7898
nakanoyuki <yukich41@gmail.com>
2019-05-26 19:04:06 +0900

    edit search_user

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,36 +1,36 @@
 $(function() {
 
-var search_user = $("#user-search-result");
-var member_user= $(".chat-group-user__name__users");
+  var search_user = $("#user-search-result");
+  var member_user= $(".chat-group-user__name__users");
 
-function appendUser(user){
+  function appendUser(user){
 
-  var html = 
-  `<div class="chat-group-user clearfix">
-  <p class="chat-group-user__name">${user.name}</p>
-  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-  </div>`
+    var html = 
+    `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${user.name}</p>
+    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    </div>`
 
-  search_user.append(html);
-}
+    search_user.append(html);
+  }
 
-function appendNoUserToHTML(msg){
-  var html = `<p class="chat-group-user__name">${msg}</p>`
-  search_user.append(html);
-}
+  function appendNoUserToHTML(msg){
+    var html = `<p class="chat-group-user__name">${msg}</p>`
+    search_user.append(html);
+  }
 
 
-function AppendUsermemberList(name,user_id){
-  var html = 
+  function AppendUsermemberList(name,user_id){
+    
+    var html = 
+    `<div class="chat-group-user clearfix js-chat-member" id="${user_id}">
+      <input name="group[user_ids][]" type="hidden" value="${user_id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
+      </div>`
 
-`<div class="chat-group-user clearfix js-chat-member" id="${user_id}">
-  <input name="group[user_ids][]" type="hidden" value="${user_id}">
-  <p class="chat-group-user__name">${name}</p>
-  <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
-  </div>`
-
- member_user.append(html);
-}
+    member_user.append(html);
+  }
 
 $(document).on("keyup","#user-search-field",function() {
     var input = $("#user-search-field").val();
