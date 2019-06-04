503cd0acf9568f17e997d0bd004461209efe11b2
nakanoyuki <yukich41@gmail.com>
2019-06-04 19:40:14 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_yuki.pem ']  
+                  keys: ['~/.ssh/key_yuki.pem']  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
